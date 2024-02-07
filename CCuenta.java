2e3e8acfdffe17f948e2c0413f8762dc02945a18
mariana <mariana.m.v611@gmail.com>
2024-02-07 19:56:10 +0100

    commit inicial del local-consola

--- CCuenta.java
+++ CCuenta.java
@@ -1,27 +1,65 @@
 package cuentas;
+/**
+ * La clase CCuenta representa una cuenta bancaria.
+ *
+ * @author Mariana
+ * @version 1.0
+ */
 public class CCuenta {
 
-
+	/**
+     * Nombre del titular de la cuenta.
+     */
     private String nombre;
+    /**
+     * Número de cuenta.
+     */
     private String cuenta;
+    /**
+     * Saldo actual de la cuenta.
+     */
     private double saldo;
+    /**
+     * Tipo de interés de la cuenta.
+     */
     private double tipoInterés;
-
+    /**
+     * Constructor por defecto de la clase CCuenta.
+     */
     public CCuenta()
     {
     }
-
+    /**
+     * Constructor parametrizado de la clase CCuenta.
+     *
+     * @param nom   Nombre del titular de la cuenta.
+     * @param cue   Número de cuenta.
+     * @param sal   Saldo inicial de la cuenta.
+     * @param tipo  Tipo de interés de la cuenta.
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
         setCuenta(cue);
         setSaldo(sal);
+        setTipoInterés(tipo);
     }
+    /**
+     * Obtiene el estado actual de la cuenta.
+     *
+     * @return Saldo actual de la cuenta.
+     */
 
     public double estado()
     {
         return getSaldo();
     }
+    /**
+     * Realiza un ingreso en la cuenta.
+     *
+     * @param cantidad Cantidad a ingresar en la cuenta. Debe ser mayor que cero.
+     * @throws Exception Si se intenta ingresar una cantidad negativa.
+     */
 
     public void ingresar(double cantidad) throws Exception
     {
@@ -29,6 +67,12 @@ public class CCuenta {
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
+    /**
+     * Realiza una retirada de dinero de la cuenta.
+     *
+     * @param cantidad Cantidad a retirar de la cuenta. Debe ser mayor que cero.
+     * @throws Exception Si se intenta retirar una cantidad negativa o si no hay suficiente saldo.
+     */
 
     public void retirar(double cantidad) throws Exception
     {
@@ -38,6 +82,7 @@ public class CCuenta {
             throw new Exception ("No se hay suficiente saldo");
         setSaldo(getSaldo() - cantidad);
     }
+ // Métodos privados para acceder a los atributos encapsulados
 
 	private String getNombre() {
 		return nombre;
